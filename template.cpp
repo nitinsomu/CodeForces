34054f7098458dd383bcd9e479670b05ae1ee116
nitinsomu <nitin.somu13@gmail.com>
2024-08-20 20:00:00 +0530

    .

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+#define ll long long
+void debugArr(vector<ll>& arr) {for(auto ele : arr) cout << ele << " ";}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+
+    }
+}
